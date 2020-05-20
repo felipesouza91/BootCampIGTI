382271ad5fadc702c63132dc4ca3c96ea4c377e1
Felipe Souza <felipedb91@gmail.com>
2020-05-19 22:56:48 -0300

    Finish class 7

--- Modulo01/aula05/js/script.js
+++ Modulo01/aula05/js/script.js
@@ -1,2 +1,10 @@
-var title = document.querySelector("h1");
-alert(title.textContent);
+var a = 2;
+var b = 4;
+
+if (a > b) {
+  alert("Maior");
+} else if (a === b) {
+  alert("Igual");
+} else {
+  alert("Menor");
+}
---
+++ Modulo01/aula07/js/script.js
@@ -0,0 +1,6 @@
+var title = document.querySelector("h1");
+title.textContent = "Teste";
+var city = document.querySelector("#city");
+city.textContent = "São Paulo";
+var personalDataArray = document.querySelectorAll(".personal-data");
+personalDataArray = Array.from(personalDataArray);
