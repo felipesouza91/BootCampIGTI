d49d3a06e91430c64b7b2d39e5bd80fe13f4a604
Felipe Souza <felipedb91@gmail.com>
2020-06-08 14:00:37 -0300

    Ajustada as funçoes sort do Array

--- Modulo02/pratico01/src/app.js
+++ Modulo02/pratico01/src/app.js
@@ -43,15 +43,34 @@ function cidadeDeMaiorNomePorEstado() {
       .sort((a, b) => b.Nome.length - a.Nome.length)
       .sort((a, b) => b.Nome - a.Nome);
 
-    cidades.push(cidadeOrder[0]);
+    cidades.push({ ...cidadeOrder[0], sigla: estado.Sigla });
   });
 
+  return cidades.sort((a, b) => b.Nome.length - a.Nome.length);
+}
+
+function cidadeDeMenoNomePorEstado() {
+  let cidades = [];
+  Estados.forEach(function (estado) {
+    const { data } = cidadesPorEstado(estado.Sigla);
+    const cidadeOrder = data
+      .sort((a, b) => a.Nome.length - b.Nome.length)
+      .sort((a, b) => a.Nome.length - b.Nome.length);
+    cidades.push({ ...cidadeOrder[0], sigla: estado.Sigla });
+  });
   return cidades
-    .sort((a, b) => b.Nome.length - a.Nome.length)
-    .sort((a, b) => b.Nome - a.Nome);
+    .sort((a, b) => {
+      if (a.Nome < b.Nome) return -1;
+      if (a.Nome > b.Nome) return 1;
+      return 0;
+    })
+    .sort((a, b) => a.Nome.length - b.Nome.length);
 }
 
 cidadesPorEstado();
 console.log(estadosComMaisCidades());
 console.log(estadosComMenosCidades());
 console.log(cidadeDeMaiorNomePorEstado());
+console.log(cidadeDeMenoNomePorEstado());
+console.log(cidadeDeMaiorNomePorEstado()[0]);
+console.log(cidadeDeMenoNomePorEstado()[0]);
